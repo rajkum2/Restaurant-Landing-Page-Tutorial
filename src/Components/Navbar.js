c61656086a26b3ea8293fe8dac1828366c20a2b5
smar602@safeway.com <Sravan.martha@albertsons.com>
2024-10-16 00:55:52 -0700

    fix nav on mobile & goole tag manager

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,93 +14,95 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import HomeIcon from "@mui/icons-material/Home";
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 // Removed unused imports
 // import { Route, Routes } from 'react-router-dom';
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
-  const menuOptions = [
-    {
-      text: "Home",
-      icon: <HomeIcon />,
-    },
-    {
-      text: "Menu",
-      icon: <HomeIcon />,
-    },
-    {
-      text: "Catering",
-      icon: <HomeIcon />,
-    },
-    // Uncomment if you plan to use these options
-    // {
-    //   text: "About",
-    //   icon: <InfoIcon />,
-    // },
-    // {
-    //   text: "Testimonials",
-    //   icon: <CommentRoundedIcon />,
-    // },
-    // {
-    //   text: "Contact",
-    //   icon: <PhoneRoundedIcon />,
-    // },
-    // {
-    //   text: "Cart",
-    //   icon: <ShoppingCartRoundedIcon />,
-    // },
-  ];
   return (
-    <nav>
-      <div className="nav-logo-container">
-        <img src={Logo} alt="" />
-        4378 Thornton Ave, Fremont, CA-94538
+    <>
+      <nav>
+        <div className="nav-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="navbar-links-container">
+          <Link to={'/'}>Home</Link>
+          {/* <Link to={'/about'}>About</Link> */}
+          <Link to={'/menu'}>Menu</Link>
+          <Link to={'/catering'}>Catering</Link>
+          {/* <a href="">About</a>
+          <a href="">Testimonials</a>
+          <a href="">Contact</a> */}
+          
+
+          
+        </div>
         
-        <button hidden className="primary-button">
-          <a href="tel:5108941238" style={{fontSize: "30px"}}><FaPhoneAlt size={25} color="red" className="navbar-cart-icon" /> 510 894-1238</a>
-          <br/> Tue - Sun | 12 PM - 11 PM
-        </button>
         
-      </div>
-      <div className="navbar-links-container">
-        <Link to={'/'}>Home</Link>
-        {/* <Link to={'/about'}>About</Link> */}
-        <Link to={'/menu'}>Menu</Link>
-        <Link to={'/catering'}>Catering</Link>
-        {/* <a href="">About</a>
-        <a href="">Testimonials</a>
-        <a href="">Contact</a> */}
+        <div className="navbar-menu-container">
+          <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+        </div>
+        <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={() => setOpenMenu(false)}
+            onKeyDown={() => setOpenMenu(false)}
+          >
+            <List>
+              
+                <ListItem key="Home" disablePadding>
+                  <Link to={'/'}>
+                    <ListItemButton>
+                      <ListItemIcon><RestaurantMenuIcon /></ListItemIcon>
+                      <ListItemText primary="Home" />
+                    </ListItemButton>
+                  </Link>
+                </ListItem>
+                <ListItem key="Menu" disablePadding>
+                  <Link to={'/menu'}>
+                    <ListItemButton>
+                      <ListItemIcon><HomeIcon /></ListItemIcon>
+                      <ListItemText primary="Menu" />
+                    </ListItemButton>
+                  </Link>
+                </ListItem>
+                <ListItem key="Catering" disablePadding>
+                  <Link to={'/catering'}>
+                    <ListItemButton>
+                      <ListItemIcon><DinnerDiningIcon /></ListItemIcon>
+                      <ListItemText primary="Catering" />
+                    </ListItemButton>
+                  </Link>
+                </ListItem>
+            </List>
+            <Divider />
+          </Box>
+        </Drawer>
+      </nav>
+      <Grid container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center">
+        <Grid item>4378 Thornton Ave, Fremont, CA-94538</Grid>
+        <Grid item>
+          <button className="primary-button">
+            <a href="tel:5108941238" style={{fontSize: "30px"}}><FaPhoneAlt size={25} color="red" className="navbar-cart-icon" /> 510 894-1238</a>
+            <br/> Tue - Sun | 12 PM - 11 PM
+          </button>
+        </Grid>
+      </Grid>
+      <div>
+        
+        
         
 
-        <button className="primary-button">
-          <a href="tel:5108941238" style={{fontSize: "30px"}}><FaPhoneAlt size={25} color="red" className="navbar-cart-icon" /> 510 894-1238</a>
-          <br/> Tue - Sun | 12 PM - 11 PM
-        </button>
       </div>
-      <div className="navbar-menu-container">
-        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-      </div>
-      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={() => setOpenMenu(false)}
-          onKeyDown={() => setOpenMenu(false)}
-        >
-          <List>
-            {menuOptions.map((item) => (
-              <ListItem key={item.text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText primary={item.text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-        </Box>
-      </Drawer>
-    </nav>
+    </>
   );
 };
 
